431072fd4198452e77154abfd5b83b2a057bb7f6
Joseph Yhu <xbhlx7@gmail.com>
2020-01-06 03:56:11 -0500

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,55 +1,115 @@
 <?php
-// Create a multidimensional array called $quotes.
-// The array should contain at least 5 associative arrays.
-// Each associative array should contain the following key-value pairs:
-//   "quote" => (str) The text of the quote itself,
-//   "source" => (str) The creator of the quote,
-//   "type" (optional) => (str) e.g., "book," "movie," etc.
-//   "citation" (optional) => (str) Where the quote comes from, e.g., book, movie, TV series, etc.,
-//   "year" (optional) => (int) The year of the quote.
-// The quotes are the top 5 quotes from the American Film Institute (AFI)'s '100 Years...100 Movie Quotes.'
-$quotes = [["quote" => "Frankly, my dear, I don't give a damn.",
+$quotes = [
+           ["quote" => "Frankly, my dear, I don't give a damn.",
             "source" => "Rhett Butler",
             "type" => "movie",
             "citation" => "Gone with the Wind",
-            "year" => 1939],
+            "year" => 1939
+           ],
            ["quote" => "I'm gonna make him an offer he can't refuse.",
             "source" => "Vito Corleone",
             "type" => "movie",
             "citation" => "The Godfather",
-            "year" => 1972],
+            "year" => 1972
+           ],
            ["quote" => "You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
             "source" => "Terry Malloy",
             "type" => "movie",
             "citation" => "On the Waterfront",
-            "year" => 1954],
+            "year" => 1954
+           ],
            ["quote" => "Toto, I've a feeling we're not in Kansas anymore.",
             "source" => "Dorothy Gale",
             "type" => "movie",
             "citation" => "The Wizard of Oz",
-            "year" => 1939],
+            "year" => 1939
+           ],
            ["quote" => "Here's looking at you, kid.",
             "source" => "Rick Blaine",
             "type" => "movie",
             "citation" => "Casablanca",
-            "year" => 1942]];
+            "year" => 1942
+           ],
+           ["quote" => "All right, Mr. DeMille, I'm ready for my close-up.",
+            "source" => "Norma Desmond",
+            "type" => "movie",
+            "citation" => "Sunset Boulevard",
+            "year" => 1950
+           ],
+           ["quote" => "May the Force be with you.",
+            "source" => "Han Solo",
+            "type" => "movie",
+            "citation" => "Star Wars",
+            "year" => 1977
+           ],
+           ["quote" => "Fasten your seatbelts. It's going to be a bumpy night.",
+            "source" => "Margo Channing",
+            "type" => "movie",
+            "citation" => "All About Eve",
+            "year" => 1950
+           ],
+           ["quote" => "You talkin' to me?",
+            "source" => "Travis Bickle",
+            "type" => "movie",
+            "citation" => "Taxi Driver",
+            "year" => 1976
+           ],
+           ["quote" => "People die when they're killed.",
+            "source" => "Emiya Shirou",
+            "type" => "visual novel",
+            "citation" => "Fate/stay night",
+            "year" => 2004
+           ],
+           ["quote" => "El Psy Kongroo.",
+            "source" => "Okabe Rintaro",
+            "type" => "visual novel",
+            "citation" => "Steins;Gate",
+            "year" => 2009
+           ],
+           ["quote" => "Deceive the first you. Deceive the world. That is the choice to arrive at the 'Steins Gate.'",
+            "source" => "Okabe Rintaro",
+            "type" => "visual novel",
+            "citation" => "Steins;Gate",
+            "year" => 2009
+           ],
+           ["quote" => "I am mad scientist. It's so cool. Son of a bitch.",
+            "source" => "Okabe Rintaro",
+            "type" => "anime",
+            "citation" => "Steins;Gate",
+            "year" => 2011
+           ],
+           ["quote" => "Explosion!",
+            "source" => "Megumin",
+            "type" => "anime",
+            "citation" => "KonoSuba: God's Blessing on This Wonderful World!",
+            "year" => 2016
+           ],
+           ["quote" => "War is peace. Freedom is slavery. Ignorance is strength.",
+            "source" => "George Orwell",
+            "type" => "book",
+            "citation" => "Nineteen Eighty-Four",
+            "year" => 1949
+           ],
+           ["quote" => "All animals are equal, but some animals are more equal than others.",
+            "source" => "George Orwell",
+            "type" => "book",
+            "citation" => "Animal Farm",
+            "year" => 1945
+           ],
+           ["quote" => "One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them.",
+            "source" => "J. R. R. Tolkien",
+            "type" => "book",
+            "citation" => "The Lord of the Rings",
+            "year" => 1954
+           ]
+          ];
 
 
-// Create a function called getRandomQuote which takes an array as a parameter.
-// Return a randomly selected element in the array by using the rand function.
 function getRandomQuote($array) {
-  return $array[rand(0, 4)];
+  return $array[rand(0, count($array))];
 }
 
-// Create a function called printQuote.
-// Create a variable called $quote and assign it the value of the getRandomQuote function, passing the global variable $quotes as an argument.
-// Create an empty string called $string.
-// Concatenate the string with the value of the key "quote,"
-// Concatenate the string with the value of the key "source,"
-// Check to see if the value of the key "type" exists. If it does, concatenate the string with the value. If it doesn't, do nothing.
-// Check to see if the value of the key "citation" exists. If it does, concantenate the string with the value. If it doesn't, do nothing.
-// Check to see if the value of the key "year" exists. If it does, concatenate the string with the value. If it doesn't, do nothing.
-// Display the full string with appropriate HTML tags.
+
 function printQuote() {
   global $quotes;
   $quote = getRandomQuote($quotes);
@@ -69,7 +129,7 @@ function printQuote() {
   echo $string;
 }
 
-// Choose a random hex value for the background color.
+
 function color() {
   $color = "#";
   $color .= dechex(rand(0, 5));
